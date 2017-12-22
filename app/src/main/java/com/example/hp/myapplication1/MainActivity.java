6fd29d485faaac89670cfc592b8bb41272a5d379
meen <smeesathit@gmail.com>
2017-12-22 15:53:05 +0700

    add button action

--- app/src/main/java/com/example/hp/myapplication1/MainActivity.java
+++ app/src/main/java/com/example/hp/myapplication1/MainActivity.java
@@ -4,16 +4,26 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    ฺButton okButton;
+    EditText editText;
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        okButton = findViewById(R.id.button);
+        editText = findViewById(R.id.editText);
+        textView = findViewById(R.id.textView);
+        Button okButton = findViewById(R.id.button);
 
         okButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = editText.getText().toString();
+                textView.setText("สวัสดี คุณ" + name);
+            }
         });
     }
 }
